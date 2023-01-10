2ddab90102674463ea1b433e36f6f07ab9e0c728
Sumanta Gogoi <sumanta@xynocast.com>
2023-01-10 12:52:24 +0530

    no message

--- src/pages/GWG/GWGINDEX.jsx
+++ src/pages/GWG/GWGINDEX.jsx
@@ -1,14 +1,58 @@
-import {Box, Container} from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 
 
 
 const GWGIndex = () => {
+  const images = [
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero1.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero2.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero3.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero4.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero5.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero6.jpg",
+  ]
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <Box sx={{ mt: 3 }}>
+      {/* <Box sx={{ display: 'flex', flexDirection: 'column', height: '50vh' }}>
+        <ImageSlider images={images} />
 
-        <img src="/assets/images/gwg.jpg" alt="A description of the image" width="100%" />
+      </Box> */}
 
-      </Box>
+      <Box sx={{ display: 'flex', mb: 4 }}>
+        <Container maxWidth='md'>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+            <Box sx={{}}>
+              {/* <img className='about-image' src='https://thegreenwoodhotels.com/wp-content/uploads/2022/06/Reception3.png' width="90%" /> */}
+            </Box>
+
+            <Typography variant='h5' sx={{ fontWeight: 'bold', fontFamily: 'Savoy Regular' }}>ABOUT THE GREENWOOD, GUWAHATI </Typography>
+
+
+            <Typography variant='body1' sx={{ mt: 2 }}>
+
+
+              Framed by the alpine
+              mountains of Meghalaya on one side and the charming city of Guwahati on the other,
+              lies the newest luxury property of Greenwood Hotels and Resorts: an urban oasis to nurture unparalleled leisure.
+              Experience this burgeoning city through a modern lens in the most luxurious accommodations this city can offer.
+
+              <br></br>  <br></br>
+              Top-rung facilities, elegant accommodations, impeccable
+              dining and around-the-clock professional management awaits you at this home away from home.
+
+            </Typography>
+
+
+
+
+
+          </Box>
+        </Container>
+      </Box >
+
+    </Box >
+
   )
 }
 
--- src/pages/GWT/GwtIndex.jsx
+++ src/pages/GWT/GwtIndex.jsx
@@ -1,15 +1,69 @@
-import {Box, Container} from '@mui/material'
-import CategoryTabs from '../../components/CatTabs/CategoryTabs'
+import { Box, Container, Typography } from '@mui/material';
+import '../home/GW.css';
+import ImageSlider from '../../components/slider/slider';
 
 
 
 const GWTIndex = () => {
-  return (
-    <Box sx={{display:'flex', flexDirection:'column'}}>
 
-     <img src="/assets/images/gwt.jpg" alt="A description of the image" width="100%" height="100%" />
-  
-  </Box>
+  const images = [
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero1.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero2.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero3.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero4.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero5.jpg",
+    "https://thegreenwoodhotels.com/wp-content/uploads/2022/09/gwr-main-hero6.jpg",
+  ]
+
+  return (
+    <Box sx={{ mt: 3 }}>
+      {/* <Box sx={{ display: 'flex', flexDirection: 'column', height: '50vh' }}>
+        <ImageSlider images={images} />
+
+      </Box> */}
+
+      <Box sx={{ display: 'flex', mb: 4 }}>
+        <Container maxWidth='md'>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+            <Box sx={{}}>
+              {/* <img className='about-image' src='https://thegreenwoodhotels.com/wp-content/uploads/2022/06/Reception3.png' width="90%" /> */}
+            </Box>
+
+            <Typography variant='h5' sx={{ fontWeight: 'bold', fontFamily: 'Savoy Regular' }}>ABOUT THE GREENWOOD HOTEL</Typography>
+
+
+            <Typography variant='body1' sx={{ mt: 2 }}>
+
+
+              Standing as one of the most prominent luxury escapes in the town of Tezpur, the ‘Cultural Capital’ of Assam, The Greenwood, Tezpur entices anyone who wishes to absorb naturesque, vibrant and fun-filled moments, just close to the bustling urban landscape of the historic town.
+              <br></br>
+              Enjoying close proximity to renowned World Heritage Sites and nature reserves less than 50 kms, such as Kaziranga National Park and Nameri National Park, as well as some of the most spectacular cultural and historical hotspots in the state.
+              <br></br>
+              This property is a quintessential replica of a retreat surrounded by beautiful hills, deep valleys and lush greenery adding an ancient touch to take you back to an era you would have only read about, you need to visit Tezpur in Assam.
+
+            </Typography>
+
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection: 'row', mt: 12 }}>
+              <img src="https://thegreenwoodhotels.com/wp-content/uploads/2022/05/gwexp.png" alt="Logo" style={{ width: '45px', height: '45px', marginRight: '8px' }} />
+              <Typography variant="h7" style={{ fontFamily: 'Savoy Regular', textTransform: 'uppercase' }}>
+                THE CONTEMPORARY AMBIENCE
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", mt: 2, alignItems: "center", justifyContent: "center", flexDirection: 'row' }} >
+              <Typography variant="body1" sx={{ fontFamily: 'Savoy Italic', width: '90%', align: "center", textAlign: "center" }}>
+                Featuring a wide selection of exquisite suites and rooms, along with elegant dining, bar and event spaces,
+                the property brings to you the lavishness and rich authenticity deeply entrenched in its remarkable history and distinct culture.
+              </Typography>
+          </Box>
+
+
+
+      </Box>
+    </Container>
+      </Box >
+
+    </Box >
+
   )
 }
 
